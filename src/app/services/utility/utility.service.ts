fda18540c59a75cacc57b4039821441827a1dc91
SunshineMocha <127298037+SunshineMocha@users.noreply.github.com>
2023-05-09 13:44:35 +0200

    commit test

--- src/app/services/utility/utility.service.ts
+++ src/app/services/utility/utility.service.ts
@@ -12,6 +12,6 @@ export class UtilityService {
   sortArrayByAlphabet(array: ITodo[]): ITodo[]{ // le funzioni che non ci servono nell'interface le spostiamo nei service o nei model, console ng g s utility per creare
     const newArray = [...array]
     newArray.sort((a, b) => a.title.localeCompare(b.title));
-    return newArray;
+    return newArray; // a
   }
 }
